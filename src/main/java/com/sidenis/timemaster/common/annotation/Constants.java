cf4c8f8ee6fad5744dfdc7a913a3f7d58ecc98c1
Kirill.Vasilyev <Kirill.Vasilyev@sidenis.com>
2018-10-01 20:03:12 +0700

    Add tests for get days by current user

--- src/main/java/com/sidenis/timemaster/common/annotation/Constants.java
+++ src/main/java/com/sidenis/timemaster/common/annotation/Constants.java
@@ -7,6 +7,11 @@ public class Constants {
     public static final String userPassword_AA = "123av";
     public static final int ID_AA = 163;
 
+    protected static final String userName_KD = "kirill.dushin";
+    protected static final String userPassword_KD = "dev09";
+    public static final int ID_KD = 165;
+
+
     public static final String super_userName = "su";
     public static final String super_userPassword = "susu";
 
--- src/main/java/com/sidenis/timemaster/common/rest/ExtraController.java
+++ src/main/java/com/sidenis/timemaster/common/rest/ExtraController.java
@@ -2,6 +2,10 @@ package com.sidenis.timemaster.common.rest;
 
 import com.sidenis.timemaster.common.annotation.BaseTimeMasterTest;
 
+import java.util.Map;
+
+import static com.jayway.restassured.RestAssured.given;
+
 //Daria
 public class ExtraController extends TimeMasterController {
 }
